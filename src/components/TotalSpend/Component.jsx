a048842cf1997ffc918a911546ac8a4a6c096cd4
Seva Arkhangelskiy <cubadacapo@gmail.com>
2017-12-18 22:21:23 -0500

    Enable eslint

--- src/components/TotalSpend/Component.jsx
+++ src/components/TotalSpend/Component.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import './styles.scss';
 
 export default class TotalSpend extends React.Component {
+  /**
+   * @returns {String}
+   */
   render () {
     return (
       <div className='total-spend'>
         Hello
       </div>
-    )
+    );
   }
 }
